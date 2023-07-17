63335fc62b596c7ed0037d297a2703c2529bce41
riuy100 <488463256@qq.com>
2023-07-17 22:21:58 +0800

    revise to pandas
    Signed-off-by: riuy100 <488463256@qq.com>

--- process.py
+++ process.py
@@ -9,7 +9,7 @@ from tkinter import ttk
 import time
 import os
 import threading
-import csv
+import pandas as pd
 
 POLLING_DELAY = 250  # ms
 FILEPATH_PLACEHOLDER = "点击此处选择文件"
@@ -39,7 +39,7 @@ class ExcelProcessorGUI:
 
         self.lock = threading.Lock()
         self.finished = True
-        self.workbook_result = None
+        self.df_out = None
         self.process_iter_max = 0
         self.process_iter = 0
 
@@ -47,51 +47,7 @@ class ExcelProcessorGUI:
             master, orient="horizontal", length=300, mode="determinate")
         self.progress_bar.grid(row=3, column=0, padx=5, pady=5)
 
-    def load_excel(self):
-        print(f"加载文件中：{self.file_path}")
-        if self.file_path.endswith(".xlsx"):
-            workbook = openpyxl.load_workbook(self.file_path)
-            sheet = workbook[0]
-            return sheet
-        elif self.file_path.endswith(".xls"):
-            # 读取 xls 文件
-            workbook = xlrd.open_workbook(self.file_path)
-            worksheet = workbook.sheet_by_index(0)
-
-            # 创建新的 xlsx 文件
-            workbook_new = openpyxl.Workbook()
-            worksheet_new = workbook_new.active
-
-            # 复制数据到新文件
-            for row in range(worksheet.nrows):
-                for col in range(worksheet.ncols):
-                    value = worksheet.cell(row, col).value
-                    worksheet_new.cell(
-                        row=row+1, column=col+1, value=value)
-            temp_path = f"./temp-{time.time()}.xlsx"
-            workbook_new.save(temp_path)
-            # 不重新加载，下面的心电图结果解析会出问题
-            workbook = openpyxl.load_workbook(temp_path)
-            os.remove(temp_path)
-            worksheet_new = workbook[0]
-            return worksheet_new
-        elif self.file_path.endswith("csv"):
-            workbook = openpyxl.Workbook()
-            worksheet = workbook.active
-            with open(self.file_path) as f:
-                reader = csv.reader(f, delimiter=':')
-                for row in reader:
-                    worksheet.append(row)
-            temp_path = f"./temp-{time.time()}.xlsx"
-            workbook.save(temp_path)
-            workbook = openpyxl.load_workbook(temp_path)
-            os.remove(temp_path)
-            worksheet_new = workbook[0]
-            return worksheet_new
-        else:
-            raise Exception
-
-    # 选择要处理的 Excel 文件
+    # 文件选择框绑定的事件回调，选择要处理的 Excel 文件
     def choose_file(self, event):
         self.process_button['text'] = "处理"
         file_path = filedialog.askopenfilename(
@@ -99,40 +55,12 @@ class ExcelProcessorGUI:
         if file_path:
             self.file_path_var.set(file_path)
             self.status_label.config(text="已选择文件：" + file_path)
+            self.process_button['state'] = tk.NORMAL
         else:
             self.file_path_var.set(FILEPATH_PLACEHOLDER)
             self.status_label.config(text="请选择要处理的 Excel 文件")
 
-    # 轮询检查任务是否已经完成
-    def check_status(self):
-        with self.lock:
-            if self.finished:
-                self.progress_bar["value"] = 0
-                self.process_button['state'] = tk.NORMAL
-                # 保存 Excel 文件
-                new_file_path = filedialog.asksaveasfilename(
-                    title="请选择保存文件路径", defaultextension=".xlsx", filetypes=[("Excel 文件", "*.xlsx")])
-                if new_file_path:
-                    self.workbook_result.save(new_file_path)
-                    self.status_label.config(text="处理完成！已保存到：" + new_file_path)
-                    self.file_path_var.set(FILEPATH_PLACEHOLDER)
-                else:
-                    self.status_label.config(text="处理完成！但未保存文件")
-                    self.process_button['text'] = "保存"
-            else:
-                # 继续轮询检查
-                self.progress_bar["maximum"] = self.process_iter_max
-                self.progress_bar["value"] = self.process_iter
-                self.master.after(POLLING_DELAY, self.check_status)
-
-    def process_thread(self):
-        # 加载 Excel 表格
-        sheet = self.load_excel()
-        self.workbook_result = self.process_inner(sheet)
-        with self.lock:
-            self.finished = True
-
-    # 处理 Excel 文件
+    # 按钮上绑定的事件回调，处理 Excel 文件
     def process_excel(self):
         self.file_path = self.file_path_var.get()
         if self.file_path == FILEPATH_PLACEHOLDER:
@@ -142,7 +70,7 @@ class ExcelProcessorGUI:
             with self.lock:
                 self.process_button['state'] = tk.DISABLED
                 self.finished = False
-                self.workbook_result = None
+                self.df_out = None
 
             t = threading.Thread(target=self.process_thread)
             t.daemon = True
@@ -152,27 +80,68 @@ class ExcelProcessorGUI:
             # 处理结束后重置进度条和状态栏
             self.status_label.config(text=f"处理出错：{e}, 请重新选择要处理的 Excel 文件")
 
-    def process_inner(self, sheet):
+
+    # 轮询检查任务是否已经完成的回调，tkinter 只有单线程
+    def check_status(self):
+        with self.lock:
+            if self.finished:
+                self.progress_bar["value"] = self.process_iter_max
+                self.process_button['state'] = tk.NORMAL
+                # 保存 Excel 文件
+                new_file_path = filedialog.asksaveasfilename(
+                    title="请选择保存文件路径", defaultextension=".xlsx", filetypes=[("Excel 文件", "*.xlsx")])
+                if new_file_path:
+                    self.df_out.to_excel(new_file_path, index=False)
+                    self.status_label.config(text="处理完成！已保存到：" + new_file_path)
+                    self.file_path_var.set(FILEPATH_PLACEHOLDER)
+                else:
+                    self.status_label.config(text="处理完成！但未保存文件")
+            else:
+                # 继续轮询检查
+                self.progress_bar["maximum"] = self.process_iter_max
+                self.progress_bar["value"] = self.process_iter
+                self.master.after(POLLING_DELAY, self.check_status)
+
+    def process_thread(self):
+        # 加载 Excel 表格
+        df = self.load_excel()
+        self.df_out = self.process_inner(df)
+        with self.lock:
+            self.finished = True
+
+    def load_excel(self):
+        print(f"加载文件中：{self.file_path}")
+        if self.file_path.endswith(".xlsx") or self.file_path.endswith("xls"):
+            df = pd.read_excel(self.file_path)
+            return df
+        elif self.file_path.endswith("csv"):
+            df = pd.read_csv(self.file_path, on_bad_lines='warn')
+            return df
+        else:
+            raise Exception
+
+    def process_inner(self, df):
+        df = df.fillna('')
         # 定义字典变量，用于记录每个人的所有检查项目和指标值
         data_dict = {}
 
         last_name = None
         last_project = None
-        print(f"开始解析该文件中的原始数据：{self.file_path}")
 
-        # 遍历 Excel 表格的每一行数据
-        rows = sheet.iter_rows(min_row=2, values_only=True)
+        # 遍历 Excel 表格的每一行数据，
         # 因为我们用 self.finished 监听了后台线程是否结束，
         # 所以事实上只会有一个后台线程，这些数据不需要加锁，不会有并发问题
-        self.process_iter_max = sheet.max_row - 1
+        self.process_iter_max = len(df)
+        print(f"共计 {self.process_iter_max} 行数据")
         self.process_iter = 0
-        for i, row in enumerate(rows):
+        for i, row in df.iterrows():
             self.process_iter = i
+            row = list(row.to_dict().values())
             name, gender, age, phone, company, project, value, interval = row[:8]
             # 按照人名进行聚合，将每个人的所有检查项目和指标值放入一个列表中
-            if project is None:
+            if project == '':
                 # 心电图项目的数据可能会到下一行
-                if last_name is not None and name is not None:
+                if last_name != '' and name == '':
                     data_dict[last_name][last_project] += name
                 continue
             last_name = name
@@ -202,14 +171,12 @@ class ExcelProcessorGUI:
         print(
             f"按姓名解析完毕，共解析到 {len(data_dict)} 人，每人 {len(headers)} 项数据，开始整理新表格")
 
-        # 创建新的 Excel 表格，并写入处理后的数据
-        workbook_new = openpyxl.Workbook()
-        worksheet_new = workbook_new.active
-        # 写入表头
-        worksheet_new.append(headers)
-        # 同理不需要加锁
-        self.process_iter_max = len(data_dict) - 1
-        self.process_iter = 0
+        # 将字典转换为 Pandas 数据帧
+        df_out = pd.DataFrame.from_dict(data_dict, orient='index')
+        # 选择需要的列
+        df_out = df_out[['姓名', '性别', '年龄', '电话', '单位'] + projects]
+        '''
+        df_out = pd.DataFrame(columns=headers)
         # 写入数据
         for i, data in enumerate(data_dict.values()):
             self.process_iter = i + 1
@@ -217,8 +184,9 @@ class ExcelProcessorGUI:
                         data["年龄"], data["电话"], data["单位"]]
             for header in projects:
                 row_data.append(data.get(header, ""))
-            worksheet_new.append(row_data)
-        return workbook_new
+            df_out.append(row_data)
+        '''
+        return df_out
 
 
 if __name__ == "__main__":
